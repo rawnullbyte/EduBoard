d1a15d457c66a09772011f09464a1d95ea00fac1
Matej Bielik <matej.bielik@proton.me>
2026-04-30 13:50:40 +0200

    Fix light theme by pointing loadEnv to project root

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '', [])
+  const env = loadEnv(mode, '../', [])
   return {
     plugins: [react()],
     define: {
